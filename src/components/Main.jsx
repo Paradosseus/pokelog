e52764451351b57c6f2d5f8a5fff398a6a2402a5
Francis Joshua Collado <colladofrancis524@gmail.com>
2023-10-24 00:46:22 +0800

    Fixed inconsistent fetching of data from Poke Api Added preview functionality and dynamic data Added Animated Sprites

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -7,103 +7,93 @@ import { Sidebar } from "./Sidebar";
 export const Main = () => {
 
     const [pokeData, setpokeData] = useState([]);
+    const [selectedPokemon, setSelectedPokemon] = useState(null);
     let arr = [];
 
     useEffect(() => {
         const getData = async () => {
-            const response = await Axios.get("https://pokeapi.co/api/v2/pokemon?limit=6")
+            const response = await Axios.get("https://pokeapi.co/api/v2/pokemon?limit=150")
             const fetchedData = await response.data.results;
 
-            fetchedData.map(async (data) => {
-                const response = await Axios.get(data.url)
-                const fetchData = await response.data;
-                arr.push(fetchData);
-                setpokeData(arr);
-
+            const promise = fetchedData.map(async (data) => {
+                const response = await Axios.get(data.url);
+                return response.data;
             });
 
+            const data = await Promise.all(promise);
 
+            setpokeData(data);
 
         }
         return () => getData();
 
     }, []);
-    console.log(pokeData);
 
+    const clickedPokemon = (id) => {
+        pokeData.map((pokemon) => {
+            if (pokemon.id === id) {
+                setSelectedPokemon(pokemon)
+                console.log(selectedPokemon);
+            }
+        })
+    }
 
-
-
-    // const pokemons = pokeData.map((pokemon) => {
-    //     return <div key={pokemon.id} className="border">
-    //         <img src={pokemon.sprites.front_default} alt="" className="w-[75px] mx-auto" />
-    //         <p className="capitalize text-center">{bulb}</p>
-    //     </div>
-    // })
 
 
 
     return (
         <div className="h-screen grid grid-rows-5">
             <div className="bg-[#7D1818] row-span-3 flex justify-center items-center">
-                <div className="bg-[#D9D9D9] bg-opacity-75 w-[350px] h-[350px] rounded-2xl">
-                    <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/132.svg" alt="" />
+                <div className="bg-[#D9D9D9] bg-opacity-75 w-[350px] h-[350px] rounded-2xl p-6">
+                    {selectedPokemon ? (
+                        <div>
+                            <div>
+                                <img src={require(`../assets/animated-sprites/${selectedPokemon.id}.gif`)} alt="" className="h-[100px] mx-auto" />
+                            </div>
+                            <div className="text-center">
+                                <h1 className="font-bold text-2xl capitalize">{selectedPokemon.name}</h1>
+                                <ul>
+                                    {selectedPokemon.types.map((typeData, index) => (
+                                        <li className="inline-block mx-1 border rounded-3xl px-3 py-1 text-xs font-semibold capitalize" key={index}>{typeData.type.name}</li>)
+                                    )}
+                                </ul>
+                                <div className="text-left text-m">
+                                    <span className="font-bold capitalize">Abilites: {selectedPokemon.abilities.map((abilityData, index) => (<span key={index}>{abilityData.ability.name}{index < selectedPokemon.abilities.length - 1 ? ', ' : ''}</span>))}</span>
+                                </div>
+                                <div className="text-left">
+                                    <div className="font-bold">Height: {selectedPokemon.height / 10 + "m"
+                                    }</div>
+                                    <div className="font-bold">Weight: {selectedPokemon.weight / 10 + "kg"}</div>
+                                </div>
+                                <div className="text-left font-bold">
+                                    <h3>Stats:</h3>
+                                </div>
+                            </div>
+                        </div>
+                    ) : (
+                        <p>Select a Pokemon</p>
+                    )}
                 </div>
             </div>
             <div className="row-span-2">
                 <div className="grid grid-cols-3 gap-4 p-2 overflow-auto bg-[#B6B6B6] h-[350px]">
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                    <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative">
-                        <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">001</span>
-                        <div className="relative mt-6">
-                            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt="" className="w-[75px] m-auto" />
-                            <p className="capitalize text-center font-bold relative">bulbasaur</p>
-                        </div>
-                    </div>
-                </div>
+                    {pokeData.map((pokemon) => {
 
+                        return (
+                            <div className="bg-[#D9D9D9] h-[140px] rounded-lg relative" key={pokemon.id} onClick={() => clickedPokemon(pokemon.id)}>
+                                <span className="text-sm round rounded-tl-lg absolute content top-0 left-0 bg-black text-white px-2">{`#${pokemon.id}`}</span>
+                                <div className="relative mt-6">
+                                    <img src={pokemon.sprites.front_default} alt="" className="w-[75px] m-auto" />
+                                    <p className="capitalize text-center font-bold relative">{pokemon.name}</p>
+                                </div>
+                            </div>
+                        );
+                    })}
+
+
+                </div>
             </div>
-        </div>
+        </div >
     );
 }
 
