52f852cd5227115f5494bd99cc1e621b1e42becc
Francis Joshua Collado <colladofrancis524@gmail.com>
2023-10-27 01:43:48 +0800

    Fixed UI bug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -57,9 +57,7 @@ export const Main = () => {
     return (
         <div>
             <div className="bg-[#FF6F40] grid grid-rows-5 lg:grid-cols-7 xl:max-w-6xl h-[800px] border-4 border-black  rounded-2xl overflow-hidden m-auto my-4 p-4">
-                <SearchBar className="z-10 absolute" />
                 <div className="row-span-3 lg:col-span-5 lg:row-span-5 p-5 lg:order-2 relative">
-
                     {/* Pokeball logo */}
                     <div className="absolute top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-0">
                         <div className="rounded-full border-black lg:w-[400px] lg:h-[400px] md:w-[325px] md:h-[325px]  h-[300px] w-[300px] bg-[#2E2E2E] relative z-0"><div className="bg-[#FF6F40] w-full lg:h-[20px] md:h-[16px] h-[12px] absolute m-0 top-[50%] translate-y-[-50%] z-0"><div className="absolute lg:h-[150px] lg:w-[150px] md:h-[120px] md:w-[120px] h-[105px] w-[105px] bg-[#FF6F40] rounded-full bg-gray m-0 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-0"><div className="absolute lg:h-[100px] lg:w-[100px] md:h-[80px] md:w-[85px] h-[70px] w-[70px] bg-[#2E2E2E] rounded-full m-0 top-[50%] left-[50%] translate-y-[-50%] translate-x-[-50%] z-10"></div></div></div></div>
