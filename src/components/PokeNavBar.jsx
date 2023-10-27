96edef9b0bfd4435f421255e5ef58b91ececbdf2
Francis Joshua Collado <colladofrancis524@gmail.com>
2023-10-27 13:21:04 +0800

    Fixed UI issues Fixed Search position in lower screens

--- src/components/PokeNavBar.jsx
+++ src/components/PokeNavBar.jsx
@@ -10,14 +10,14 @@ export const PokeNavBar = (props) => {
                 <div className="py-4 hidden lg:block">
                     <img src={pokelogLogo} alt="" className="lg:w-[175px] m-auto" />
                 </div>
-                <div className="hidden lg:block"><input type="text" className="bg-[#2E2E2E] w-full px-4 py-2 border-black focus:outline-none text-white placeholder-slate-300" placeholder="Search pokemon..." onChange={(e) => setSearchInput(e.target.value)} /></div>
-                <ul className="grid lg:block grid-cols-3 lg:grid-cols-none gap-[4px] p-2 overflow-scroll scrollbar-hide bg-[#FF6F40] h-[350px] lg:h-[610px]" style={{ gridTemplateRows: "auto" }}>
+                <div className="w-full"><input type="text" className="bg-[#2E2E2E] w-full px-4 py-2 border-black focus:outline-none text-white placeholder-slate-300" placeholder="Search pokemon..." onChange={(e) => setSearchInput(e.target.value)} /></div>
+                <ul className="grid lg:block grid-cols-3 lg:grid-cols-none gap-[4px] p-2 overflow-scroll scrollbar-hide bg-[#FF6F40] h-[250px] lg:h-[610px]">
                     {props.pokemons.filter((pokemon, index) => {
                         return searchInput.toLowerCase() === '' ? pokemon : pokemon.name.toLowerCase().includes(searchInput)
                     }).map((pokemon, index) => {
                         return (
-                            <li className="bg-black h-[140px] md:h-[160px] lg:h-[85px] rounded-lg relative cursor-pointer" key={pokemon.id} onClick={() => props.clickedPokemon(pokemon.id)}>
-                                <div className="bg-[#ff5503] text-sm round rounded-tl-lg absolute content top-0 left-0 lg:bottom-0 text-white px-2 md:text-[16px] md:px-[10px] md:py-[2px] lg:flex lg:w-[60px]"><div className="lg:m-auto">{`#${pokemon.id}`}</div></div>
+                            <li className="bg-black h-[140px] md:h-[160px] lg:h-[64px] rounded-xl relative cursor-pointer" key={pokemon.id} onClick={() => props.clickedPokemon(pokemon.id)}>
+                                <div className="bg-[#ff5503] text-sm round lg:rounded-l-lg rounded-tl-lg absolute content top-0 left-0 lg:bottom-0 text-white px-2 md:text-[16px] md:px-[10px] md:py-[2px] lg:flex lg:w-[60px]"><div className="lg:m-auto">{`#${pokemon.id}`}</div></div>
                                 <div className="relative mt-6 lg:mx-auto lg:w-[200px] lg:ml-[65px] lg:mt-[12px]">
                                     <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} alt="" className="w-[75px] md:w-[90px] m-auto lg:inline-block lg:w-[60px]" />
                                     <p className="capitalize text-center font-bold relative md:text-xl lg:inline-block text-white">{pokemon.name}</p>
